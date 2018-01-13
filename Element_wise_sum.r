ec5f42c2ba150fed100bf92ad6f50bc1f9fe60e4
UDAY ALLU <udayallu2010@gmail.com>
2018-01-13 14:05:17 +0530

    Create Element_wise_sum.r

---
+++ Element_wise_sum.r
@@ -0,0 +1,15 @@
+# A_vector and B_vector have already been defined for you
+A_vector <- c(1, 2, 3)
+B_vector <- c(4, 5, 6)
+
+# Take the sum of A_vector and B_vector: total_vector
+total_vector= c(1, 2, 3) + c(4, 5, 6)
+  
+# Print total_vector
+total_vector
+
+# Calculate the difference between A_vector and B_vector: diff_vector
+diff_vector=c(1, 2, 3) - c(4, 5, 6)
+
+# Print diff_vector
+diff_vector
